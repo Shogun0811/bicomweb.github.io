b380726246f3d7485a2991daf282f5995a7cffbd
Shogun0811 <167612797+Shogun0811@users.noreply.github.com>
2024-08-07 09:49:16 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let links =document.querySelectorAll("nav li")
+icons.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+links.forEach((links)=>{
+  links.addEventListener("click",()=>{
+    nav.classList.remove("active");
+  } );
+});
